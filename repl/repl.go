cc8136658d071456525e5435c473b40d818e0487
al <bnachoff.tanabe@keio.jp>
2020-01-02 17:51:16 +0900

    implement parser (chap-2)

--- repl/repl.go
+++ repl/repl.go
@@ -6,6 +6,7 @@ import (
 	"io"
 
 	"github.com/al-keio/monkey-go/lexer"
+	"github.com/al-keio/monkey-go/parser"
 	"github.com/al-keio/monkey-go/token"
 )
 
@@ -24,9 +25,26 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
+		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
 
 		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
 			fmt.Printf("%+v\n", tok)
 		}
+	}
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
